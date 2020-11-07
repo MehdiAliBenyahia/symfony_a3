732ae9411fce95dd6807e1133e64e2d2becb7cec
mehdialibenyahia <mehdi.ali_benyahia@edu.devinci.fr>
2020-11-08 00:16:56 +0100

    Initialization project
    - Entities
    - Auth (Login / Register / Logout)
    - Controller : Admin, Main
    - CRUD (Category, Classes)

---
+++ src/Entity/Category.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $label;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Classes::class, mappedBy="category")
+     */
+    private $classes;
+
+    public function __construct()
+    {
+        $this->classes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Classes[]
+     */
+    public function getClasses(): Collection
+    {
+        return $this->classes;
+    }
+
+    public function addClass(Classes $class): self
+    {
+        if (!$this->classes->contains($class)) {
+            $this->classes[] = $class;
+            $class->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeClass(Classes $class): self
+    {
+        if ($this->classes->removeElement($class)) {
+            // set the owning side to null (unless already changed)
+            if ($class->getCategory() === $this) {
+                $class->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+}
